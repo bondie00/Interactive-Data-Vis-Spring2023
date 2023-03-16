de7c89ff37fc2c975e0a39b2b740fc9146109566
bondie00 <35945154+bondie00@users.noreply.github.com>
2023-03-16 19:24:36 -0400

    Tutorial 2_1 and 2_2

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,45 +1,113 @@
-
-/* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+const width = window.innerWidth * 0.7,
+height = window.innerHeight * 0.7,
+  margin = 65;
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType) //auto type will read data and try to turn numbers into ints
-  .then(data => {
-    console.log("data", data)
+d3.csv("../data/squirrelActivities.csv", d3.autoType).then(Squirrel => {
+  console.log("data", Squirrel)
 
 
-    //dot looks for class, pound looks for id
-  const table = d3.select("#container")
-    .append("table")
+  //dot looks for class, pound looks for id
+const table = d3.select("#container")
+  .append("table")
+  
+  table.append("thead")
+  table.append("tbody")
+    .append("tr")
+    .attr('class','row')
+
+  const tobdy = table.append("tbody")
+
+  const row = tobdy.selectAll("tr")
+    .data(Squirrel)
+    .join("tr")
+    .attr("activity", "count")
+
+    row.append("td")
+      .text(data => data.activity)
     
-    table.append("thead")
-    table.append("tbody")
-      .append("tr")
-      .attr('class','row')
+    row.append("td")
+      .text(data => data.count)
 
-    const tobdy = table.append("tbody")
 
-    const row = tobdy.selectAll("tr")
-      .data(data)
-      .join("tr")
-      .attr("class", "student")
-      .attr("id", data => data.Last)
+  /* SCALES */
+  /** This is where you should define your scales from data to pixel space */
+  const yScale = d3.scaleBand()
+    .domain(['running', 'chasing', 'climbing', 'eating', 'foraging'])
+    .range([margin, height - margin])
+    .padding(0.2)
 
-      row.append("td")
-        .text(data => data.Last)
+  const xScale = d3.scaleLinear()
+    .domain([0, Math.max(...Squirrel.map(d => d.count))])
+    .range([margin, width - margin])
+
+
+
+  /* HTML ELEMENTS */
+  /** Select your container and append the visual elements to it */
+  const svg = d3.select("#container").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+
+  svg.selectAll("rect.bar")
+    .data(Squirrel)
+    .join("rect")
+    .attr("class", "bar")
+    .attr("y", d => yScale(d.activity))
+    .attr("x", margin)
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d.count) - margin)
+
+    const yAxis = d3.axisLeft(yScale)
+    const xAxis = d3.axisBottom(xScale)
+
+    svg
+      .append("g")
+      .attr("class", "x-axis")
+      .style("transform", `translate(0px, ${height - margin}px)`)
+      .call(xAxis)
+
+      svg
+        .append("text")
+        .attr("class", "xAxisLabel")
+        .attr("y", height - margin + 35)
+        .attr("x", width/2)
+        .text("Count (number of times observed)")
+        .attr("fill", "black")
+        .attr("text-anchor", "middle")
+
+
+    svg
+      .append("g")
+      .attr("class", "y-axis")
+      .style("transform", `translate(${margin}px, 0px)`)
+      .call(yAxis)
+
+      svg
+        .append("text")
+        .attr("class", "yAxisLabel")
+        .attr("y", 12)
+        .attr("x", 0 - height/2)
+        .attr("transform", "rotate(-90)")
+        .text("Squirrel Activity")
+        .attr("fill", "black")
+        .attr("text-anchor", "middle")
       
-      row.append("td")
-        .text(data => data.First)
 
+      svg.selectAll("text.dataLabel")
+        .data(Squirrel)
+        .join("text")
+        .attr("class", "dataLabel")
+        .text(d => d.count)
+        .attr("x", d => xScale(d.count) + 15)
+        .attr("y", d => yScale(d.activity) + yScale.bandwidth() / 2)
+        .attr("fill" , "black")
+        .attr("font-size", "9pt")
+        .attr("text-anchor", "middle")
 
-
-
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
     
 
-    /* HTML ELEMENTS */
-    /** Select your container and append the visual elements to it */
 
-  })
+
+
+})
--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,104 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.9,
+  height = window.innerHeight * 0.8,
+  margin = { top: 60, bottom: 40, left: 60, right: 20 }
+  //radius = ;
 
 /* LOAD DATA */
-d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
+d3.csv("../data/cowboys_cleaned.csv", d3.autoType)
   .then(data => {
-    console.log(data)
 
-    /* SCALES */
+    const svg = d3.select("#container")
+      .append('svg')
+      .attr("width", width)
+      .attr("height", height)
+      
+    const xScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.cowboys_score))
+      .range([margin.left, width - margin.right])
     
-    /* HTML ELEMENTS */
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.pass_rating))
+      .range([height - margin.bottom, margin.top])
+
+    const rScale = d3.scaleSqrt()
+      .domain(d3.extent(data, d => d.point_difference))
+      .range([4, 15])
     
+     const colorScale = d3.scaleOrdinal()
+       .domain(["win", "lose"])
+       .range(["blue", "red"])
+    
+    const xAxis = d3.axisBottom(xScale)  
+    svg
+      .append("g")
+      .attr("class", "x-axis")
+      .style("transform", `translate(0px, ${height - margin.bottom}px)`)
+      .call(xAxis)
+
+    svg
+      .append("text")
+      .attr("class", "xAxisLabel")
+      .style("transform", `translate(${width/2}px, ${height - 5}px)`)
+      .text("Points scored by Cowboys")
+      .attr("fill", "black")
+      .attr("text-anchor", "middle")
+    
+    const yAxis = d3.axisLeft(yScale)
+    svg
+      .append("g")
+      .attr("class", "y-axis")
+      .style("transform", `translate(${margin.left}px, 0px)`)
+      .call(yAxis)
+    
+      svg
+        .append("text")
+        .attr("class", "yAxisLabel")
+        .attr('transform', 'translate('+(margin.left - 35)+',' +height/2+ ') rotate(-90)')
+        .text("Quaterback passer rating")
+        .attr("fill", "black")
+        .attr("text-anchor", "middle")
+        
+      svg
+        .append("text")
+        .attr("class", "title")
+        .text("Dallas Cowboys' team performance by QB performance")
+        .style("transform", `translate(${width/2}px, ${margin.top - 20}px)`)
+        .attr("fill", "black")
+        .attr("text-anchor", "middle")
+        .attr("font-size", "18pt")
+      
+      svg
+        .append("text")
+        .attr("class", "key")
+        .text("blue = game won")
+        .style("transform", `translate(${width - margin.right - 140}px, ${height - margin.bottom - 90}px)`)
+        .attr("fill", "black")
+        .attr("text-anchor", "start")
+        .attr("font-size", "10pt")
+        svg
+        .append("text")
+        .attr("class", "key")
+        .text("red = game lost")
+        .style("transform", `translate(${width - margin.right - 140}px, ${height - margin.bottom - 75}px)`)
+        .attr("fill", "black")
+        .attr("text-anchor", "start")
+        .attr("font-size", "10pt")
+        svg
+        .append("text")
+        .attr("class", "key")
+        .text("dot size = win/loss margin")
+        .style("transform", `translate(${width - margin.right - 140}px, ${height - margin.bottom - 60}px)`)
+        .attr("fill", "black")
+        .attr("text-anchor", "start")
+        .attr("font-size", "10pt")
+    
+    const dots = svg 
+      .selectAll("circle.dataPoint")
+      .data(data)
+      .join("circle")
+      .attr("class", "dataPoint")
+      .style("transform", d => `translate(${xScale(d.cowboys_score)}px, ${yScale(d.pass_rating)}px)`)
+      .attr("r", d => rScale(d.point_difference))
+      .attr("fill", d => colorScale(d.cowboys_win))
   });
